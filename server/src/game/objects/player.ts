088c2cb46a42bbfb06292e94527cf83648690fa2
leia uwu <leia@tutamail.com>
2025-08-01 17:50:57 -0300

    refactor: remove pointless {player/group}Status object

--- server/src/game/objects/player.ts
+++ server/src/game/objects/player.ts
@@ -2311,7 +2311,7 @@ export class Player extends BaseGameObject {
                 this.game.logger.warn("Too many new player statuses!", statuses.length);
                 statuses = statuses.slice(0, 255);
             }
-            updateMsg.playerStatus.players = statuses;
+            updateMsg.playerStatus = statuses;
             updateMsg.playerStatusDirty = true;
             player.playerStatusTicker = 0;
         }
@@ -2330,7 +2330,7 @@ export class Player extends BaseGameObject {
                 this.game.logger.warn("Too many new group statuses!", statuses.length);
                 statuses = statuses.slice(0, 255);
             }
-            updateMsg.groupStatus.players = statuses;
+            updateMsg.groupStatus = statuses;
             updateMsg.groupStatusDirty = true;
         }
 
