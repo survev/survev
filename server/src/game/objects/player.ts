50d1fa4dfb52741dba9eca340bc42762d57fb4e4
IOMAN <ioman2705@gmail.com>
2025-08-08 06:31:31 +0300

    fix: promo items dont overwrite if curr is higher

--- server/src/game/objects/player.ts
+++ server/src/game/objects/player.ts
@@ -898,6 +898,13 @@ export class Player extends BaseGameObject {
                     this.scope = key;
                 }
 
+                if (
+                    invDef.type == "heal" || invDef.type == "boost" || invDef.type == "throwable" || invDef.type == "ammo"
+                ) {
+                    this.inventory[key] = Math.max(this.inventory[key], value);
+                    continue;
+                }
+
                 this.inventory[key] = value;
             }
 
