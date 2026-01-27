8425308c644e5345e76358ece0f8e4b902d560fe
chickenPoo <ivanemerald1209@gmail.com>
2026-01-27 15:22:23 -0500

    feat (modding): add a console log after ModAPI init

--- client/src/modding/ModAPIInstance.ts
+++ client/src/modding/ModAPIInstance.ts
@@ -4,4 +4,6 @@ const modAPI: ModAPI = window.survevModAPI ?? createModAPI();
 
 window.survevModAPI = modAPI;
 
+console.log("Come view the survevModAPI docs at (add the link here later...)");
+
 export { modAPI };
