92a89bf41a10bb77a84372f5fa3414a99f36357e
Abubu-ok <fax-dainty-grunge@duck.com>
2026-04-23 12:21:13 +0700

    revert the previous commit
    just found out that pattern also include night, negative, and negotiate etc. so I remove it.

--- server/src/utils/serverHelpers.ts
+++ server/src/utils/serverHelpers.ts
@@ -154,7 +154,7 @@ const badWordsdataSet = new DataSet<{ originalWord: string }>()
             .addPattern(pattern`chigger`)
             .addPattern(pattern`chigga`)
             .addPattern(pattern`n199a`)
-            .addPattern(pattern`|n[?]g[g][?]`)
+            .addPattern(pattern`n[i]ga`)
             .addPattern(pattern`natehigg`),
     )
     .addPhrase((phrase) =>
