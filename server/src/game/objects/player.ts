2eae18840f19922de41d9e22bc48e27bf158c9e4
InkyDarkBird <alex.f.guo@gmail.com>
2026-06-03 17:45:31 -0400

    feat: allow lone survevrs to ping each other
    lint

--- server/src/game/objects/player.ts
+++ server/src/game/objects/player.ts
@@ -2641,7 +2641,9 @@ export class Player extends BaseGameObject {
 
                 // faction team leader
                 if (
-                    (emotePlayer.role === "leader" || emotePlayer.role === "captain")
+                    (emotePlayer.role === "leader"
+                        || emotePlayer.role === "captain"
+                        || emotePlayer.role === "last_man")
                     && emotePlayer.teamId === player.teamId
                 ) {
                     return true;
