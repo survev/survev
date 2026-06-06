d76a31cc0838819f5620b38438eb4cc95ec69a30
RandomNoSkin <103935551+RandomNoSkin@users.noreply.github.com>
2026-06-06 22:48:43 +0200

    Update UserRouter.ts

--- server/src/api/routes/user/UserRouter.ts
+++ server/src/api/routes/user/UserRouter.ts
@@ -318,8 +318,9 @@ UserRouter.post("/get_pass", validateParams(zGetPassRequest), async (c) => {
         matchXp += stat.damage * xpMultiplier.damage;
         matchXp += (stat.rank === 1 ? 1 : 0) * xpMultiplier.win;
         matchXp += stat.timeAlive * xpMultiplier.timeSurvived;
-        totalXp += Math.floor(matchXp * boost);
+        totalXp += matchXp * boost;
     }
+    totalXp = Math.floor(totalXp);
     console.log(`User ${user.username} earned ${totalXp} XP from ${stats.length} matches since last update`);
 
     const newTotalXp = currentXp + totalXp;
