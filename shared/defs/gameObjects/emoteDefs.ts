01f07ddfb011675c39f8e1b5bde54d938896b5d2
The-Overseer1 <astreaum9@gmail.com>
2026-08-28 18:12:09 -0400

    fix: lint
    Squash this with the Antisocial emote commit

--- shared/defs/gameObjects/emoteDefs.ts
+++ shared/defs/gameObjects/emoteDefs.ts
@@ -1803,7 +1803,7 @@ export const EmotesDefs: Record<string, EmoteDef> = {
         sound: "emote_01",
         channel: "ui",
         teamOnly: false,
-        category: EmoteCategory.Other
+        category: EmoteCategory.Other,
     },
 
     // Pass 2
