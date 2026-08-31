cb91551feb30b224fd26c3db9853b02b090e5865
ei-pi <91853103+ei-pi@users.noreply.github.com>
2026-08-31 14:00:51 -0400

    refactor: trick_chatty min/maxInterval

--- shared/defs/gameObjects/perkDefs.ts
+++ shared/defs/gameObjects/perkDefs.ts
@@ -113,6 +113,10 @@ export const PerkProperties = {
         speedMult: 1.4,
         distanceMult: 1.3,
     },
+    trick_chatty: {
+        minInterval: 5,
+        maxInterval: 15,
+    },
     trick_drain: {
         bleedTickRateMult: 3,
     },
