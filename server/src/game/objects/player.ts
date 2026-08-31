acad27ecc433be3aafcba23b15723018ada49be0
ei-pi <91853103+ei-pi@users.noreply.github.com>
2026-08-31 14:14:12 -0400

    refactor: small_arms gunEquipSpeed

--- server/src/game/objects/player.ts
+++ server/src/game/objects/player.ts
@@ -4703,7 +4703,7 @@ export class Player extends BaseGameObject {
         if (this.weaponManager.meleeAttacks.length == 0) {
             let equipSpeed = weaponDef.speed.equip;
             if (this.hasPerk("small_arms") && weaponDef.type == "gun") {
-                equipSpeed = 1;
+                equipSpeed = PerkProperties.small_arms.gunEquipSpeed;
             }
 
             this.speed += equipSpeed;
