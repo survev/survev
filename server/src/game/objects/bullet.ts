95d16486093718d4cd95128043b7ef7f6ad7070f
ei-pi <91853103+ei-pi@users.noreply.github.com>
2026-09-03 13:00:32 -0400

    fix: rename incomingDamageMult -> damageMult

--- server/src/game/objects/bullet.ts
+++ server/src/game/objects/bullet.ts
@@ -619,7 +619,7 @@ export class Bullet {
 
                     let multiplier = 1;
                     if (isHighValueTarget) {
-                        multiplier *= PerkProperties.targeting.incomingDamageMult;
+                        multiplier *= PerkProperties.targeting.damageMult;
                     }
 
                     this.bulletManager.damages.push({
