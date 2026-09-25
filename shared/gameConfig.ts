b266503f986c37e11ba8c60d9e25f8c0560f5e95
leia uwu <leia@tutamail.com>
2026-09-25 02:16:09 -0300

    chore: bump protocol version

--- shared/gameConfig.ts
+++ shared/gameConfig.ts
@@ -155,7 +155,7 @@ export const GameConfig = {
     // the protocol we originated from was 78
     // remember to bump this every time a serialization function is changed
     // or a definition item added, removed or moved
-    protocolVersion: 1026,
+    protocolVersion: 1027,
     Action,
     Anim,
     DamageType,
